6e400701742d80c5ba2cf9b100aa06a1779e99bf
VladimirKozlov73 <postwsz@mail.ru>
2025-11-02 18:45:57 +0300

    дз "Управление схемами баз данных " исправлены ошибки.

--- scripts423.sql
+++ scripts423.sql
@@ -1,8 +1,7 @@
 SELECT s.name, s.age, f.name AS faculty_name
 FROM student s
-INNER JOIN faculty f ON s.faculty_id = f.id;
+LEFT JOIN faculty f ON s.faculty_id = f.id;
 
-SELECT s.name, s.age, f.name AS faculty_name
+SELECT s.name, s.age
 FROM student s
-INNER JOIN faculty f ON s.faculty_id = f.id
 INNER JOIN avatar a ON a.student_id = s.id;
